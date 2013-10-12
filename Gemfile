53a300a306883366ab6fea4eb05a93c32df5ab76
Jorge Núñez <jorgeng87@gmail.com>
2013-10-13 00:06:14 +0200

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,29 @@
+source 'https://rubygems.org'
+
+# Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
+gem 'rails', '4.0.0'
+
+gem 'rails-api', '0.1.0'
+
+group :development, :test do
+  gem 'rspec-rails', '2.14.0'
+  gem 'sqlite3','1.3.8'
+  gem 'factory_girl_rails','4.2.0'
+  gem 'pry'
+end
+
+
+# To use ActiveModel has_secure_password
+# gem 'bcrypt-ruby', '~> 3.0.0'
+
+# To use Jbuilder templates for JSON
+# gem 'jbuilder'
+
+# Use unicorn as the app server
+# gem 'unicorn'
+
+# Deploy with Capistrano
+# gem 'capistrano', :group => :development
+
+# To use debugger
+# gem 'ruby-debug19', :require => 'ruby-debug'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_toke to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+TimeTracker::Application.config.secret_token = '5afe4cdc77211962b505df458486fd405e6557459aaa84d0be5298d06de58b4798966371cd49829f7427db3c0e0a845e3998acf097bd13b22fd3d991e7ef3218'
