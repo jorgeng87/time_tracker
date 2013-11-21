70e6d4103a16fafd793598aff23906a50bec1986
Jorge Núñez <jorgeng87@gmail.com>
2013-11-21 23:57:43 +0100

    Add tests for SessionsController

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -2,4 +2,25 @@ require 'spec_helper'
 
 describe SessionsController do
 
+  describe "POST create" do
+    describe "with valid params" do
+      it "should return an auth_token" do
+        user = create(:user, password: "12345678")
+        post :create, username: user.email, password: "12345678"
+
+        response.status.should == 201
+        JSON.parse(response.body)["auth_token"].should == AuthToken.last.token
+      end
+
+    end
+
+    describe "with invalid params" do
+    end
+
+  end
+
+  describe "DELETE destroy" do
+
+  end
+
 end
