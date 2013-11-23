53d7d61b5262cd28d7540dafe2fc0121064b8fb3
Jorge Núñez <jorgeng87@gmail.com>
2013-11-23 22:42:06 +0100

    Update DB schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131112224841) do
+ActiveRecord::Schema.define(version: 20131123184143) do
 
   create_table "auth_tokens", force: true do |t|
     t.integer  "user_id"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20131112224841) do
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "description"
   end
 
   create_table "users", force: true do |t|
