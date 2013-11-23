f22468c560da5cd58fd66220b02e6e4283ecce60
Jorge Núñez <jorgeng87@gmail.com>
2013-11-23 22:43:25 +0100

    Update Tracking factory

--- spec/factories/trackings.rb
+++ spec/factories/trackings.rb
@@ -4,6 +4,7 @@ FactoryGirl.define do
   factory :tracking do
     start "2013-10-13 00:13:54"
     stop "2013-10-13 00:13:54"
+    description "Working on project A wireframes"
     association :user 
   end
 end
