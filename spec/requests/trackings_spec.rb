b4b4b23f6bf32003c46e3d28779da4573807eaf6
Jorge Núñez <jorgeng87@gmail.com>
2013-11-24 00:29:27 +0100

    Comment unused test

--- spec/requests/trackings_spec.rb
+++ spec/requests/trackings_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe "Trackings" do
   describe "GET /trackings" do
     it "works! (now write some real specs)" do
+      pending
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       get trackings_path
       response.status.should be(200)
