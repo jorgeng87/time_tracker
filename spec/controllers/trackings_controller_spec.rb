a50bfa0a96726327d4657bdebf607ba284699ebd
Jorge Núñez <jorgeng87@gmail.com>
2013-11-23 23:36:06 +0100

    Add tests for index and destroy from TrackingsController

--- spec/controllers/trackings_controller_spec.rb
+++ spec/controllers/trackings_controller_spec.rb
@@ -3,6 +3,37 @@ require 'spec_helper'
 describe TrackingsController do
 
   describe "GET index" do
+    describe "with valid params" do
+      it "should return a listing of all trackings" do
+        user             = create(:user)
+        auth_token       = create(:auth_token, user: user)
+        tracking         = create(:tracking, user: user, description: "Doing something")
+        another_tracking = create(:tracking, user: user, description: "Doing another thing")
+
+        get 'index', { auth_token: auth_token.token } 
+        response.status.should == 200
+        response.body.should == Tracking.all.to_json
+      end
+      it "should return only the trackings from the current_user" do
+        user                  = create(:user)
+        auth_token            = create(:auth_token, user: user)
+        another_user          = create(:user)
+        tracking              = create(:tracking, user: user, description: "Doing something")
+        another_tracking      = create(:tracking, user: user, description: "Doing another thing")
+        another_user_tracking = create(:tracking, user: another_user, description: "Working out")
+
+        get 'index', { auth_token: auth_token.token } 
+        response.status.should == 200
+        response.body.should == 
+               [tracking.attributes, another_tracking.attributes].to_json
+      end
+    end
+    describe "with invalid params" do
+      it "should require authentication" do
+        get 'index' 
+        response.status.should == 401 # Unauthorized
+      end
+    end
   end
 
   describe "GET show" do
@@ -107,6 +138,34 @@ describe TrackingsController do
   end
 
   describe "DELETE destroy" do
+    describe "with valid params" do
+      it "should delete the tracking" do
+        user         = create(:user)
+        auth_token   = create(:auth_token, user: user, token: "firstUserToken")
+        tracking     = create(:tracking, user: user)
+
+        delete 'destroy', { auth_token: auth_token.token,
+                            id: tracking.id }
+        response.status.should == 204 # No content
+        Tracking.find_by_id(tracking.id).should == nil
+      end
+    end
+    describe "with invalid params" do
+      it "should require authentication" do
+        delete 'destroy', { id: 999 } 
+        response.status.should == 401 # Unauthorized
+      end
+      it "shouldn't be able to delete a tracking that doesn't belong to the current_user " do
+        user         = create(:user)
+        auth_token   = create(:auth_token, user: user, token: "firstUserToken")
+        another_user = create(:user)
+        tracking     = create(:tracking, user: another_user)
+
+        delete 'destroy', { auth_token: auth_token.token,
+                            id: tracking.id }
+        response.status.should == 403 # Forbiden
+      end
+    end
   end
 
 end
