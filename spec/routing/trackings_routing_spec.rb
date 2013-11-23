42949b9335b15b9ff38c61b37dbbcf7247032fdc
Jorge Núñez <jorgeng87@gmail.com>
2013-11-24 00:27:21 +0100

    Fix routing specs

--- spec/routing/trackings_routing_spec.rb
+++ spec/routing/trackings_routing_spec.rb
@@ -4,31 +4,23 @@ describe TrackingsController do
   describe "routing" do
 
     it "routes to #index" do
-      get("/trackings").should route_to("trackings#index")
-    end
-
-    it "routes to #new" do
-      get("/trackings/new").should route_to("trackings#new")
+      get("/trackings").should route_to("trackings#index", format: :json)
     end
 
     it "routes to #show" do
-      get("/trackings/1").should route_to("trackings#show", :id => "1")
-    end
-
-    it "routes to #edit" do
-      get("/trackings/1/edit").should route_to("trackings#edit", :id => "1")
+      get("/trackings/1").should route_to("trackings#show", :id => "1", format: :json)
     end
 
     it "routes to #create" do
-      post("/trackings").should route_to("trackings#create")
+      post("/trackings").should route_to("trackings#create", format: :json)
     end
 
     it "routes to #update" do
-      put("/trackings/1").should route_to("trackings#update", :id => "1")
+      put("/trackings/1").should route_to("trackings#update", :id => "1", format: :json)
     end
 
     it "routes to #destroy" do
-      delete("/trackings/1").should route_to("trackings#destroy", :id => "1")
+      delete("/trackings/1").should route_to("trackings#destroy", :id => "1", format: :json)
     end
 
   end
